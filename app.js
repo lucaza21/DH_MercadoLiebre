8972ecedacbd43de92bba8817418d3c6f1127b3b
obiito21 <luis.carrasquilla.z@gmail.com>
2022-05-10 21:16:12 -0500

    cambios bruscos, login, register

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ app.get('/home', (req,res) => {
     let home = path.resolve(__dirname, "./index.html");
     res.sendFile(home);
 })
+
+app.get('/register', (req,res) => {
+    let register = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(register);
+})
+app.post('/register', (req,res) => {
+    res.send(" se ha procesado el formulario")
+})
+
+
+app.get('/login', (req,res) => {
+    let login = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(login);
+})
+app.post('/login', (req,res) => {
+    res.send(" se ha iniciado sesión")
+
+})
